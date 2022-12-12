01817f62410173ff933d43dfab3425417c12a0c6
Edward Shanahan <edwardshanahan357@gmail.com>
2022-12-12 20:17:36 +0000

    Practicing declaring variables in JS and Python

---
+++ app.js
@@ -0,0 +1,12 @@
+// To declare a variable
+let hello = null;
+
+// Declare a string
+let greet = "Hello";
+
+// Declare a number
+let num = 10;
+
+// Declare a boolean
+let bool = true;
+console.log(bool);
---
+++ app.py
@@ -0,0 +1,14 @@
+# Declare a variable
+hello = None
+
+# Declare a string
+greet = "Hello"
+
+# Declare a number 
+num = 10
+
+# Declare a boolean
+
+bool = True
+
+print(bool)
